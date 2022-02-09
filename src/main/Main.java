bccc94dcd00cbf036eca34499668610d08aaf90d
joacotol <joacochrist@gmail.com>
2022-02-09 18:16:48 -0500

    Rewrote the cinema visualization code to make it customizable

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,35 @@
 package main;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Visualisation of the Cinema
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        System.out.println("1 S S S S S S S S");
-        System.out.println("2 S S S S S S S S");
-        System.out.println("3 S S S S S S S S");
-        System.out.println("4 S S S S S S S S");
-        System.out.println("5 S S S S S S S S");
-        System.out.println("6 S S S S S S S S");
-        System.out.println("7 S S S S S S S S");
+        Scanner userInput = new Scanner(System.in);
+
+        //
+        System.out.print("Enter number of rows: ");
+        int rows = userInput.nextInt();
+        System.out.print("Enter the number of seats per row: ");
+        int numberOfSeats = userInput.nextInt();
+        System.out.println("");
+        int rowCounter = 1;
+
+        System.out.print("  ");
+        // Will Loop to print the number of columns
+        for(int column = 1; column <= numberOfSeats; column++) {
+            System.out.print(column + " ");
+        }
+        System.out.println("");
+        // Loops until the number of rows are printed
+        for(int row = 0; row < rows; row++) {
+            System.out.print(rowCounter + " ");
+            // Loops until the number of seats are printed
+            for(int seat = 1; seat <= numberOfSeats; seat++) {
+                System.out.print("S ");
+            }
+            System.out.println("");
+            rowCounter++;
+        }
+
     }
 }
