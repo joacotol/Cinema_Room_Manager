79dff9c2e4a7ae3886ac77d2bf29bec542de4223
joacotol <joacochrist@gmail.com>
2022-02-17 18:32:06 -0500

    Added part 3/5 of the project

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,6 +16,7 @@ public class Main {
 
         int rowCounter = 1;
 
+        System.out.println("Cinema:");
         System.out.print("  ");
         // Will Loop to print the number of columns
         for(int column = 1; column <= numberOfSeats; column++) {
@@ -32,10 +33,20 @@ public class Main {
             System.out.println("");
             rowCounter++;
         }
+        System.out.println();
+
+        // This is for when the user chooses the seat in the cinema
+        System.out.println("Enter a row number:");
+        int chosenRow = userInput.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int chosenSeat = userInput.nextInt();
+        System.out.println();
+
 
         // Part Two: Total Income Made
         final int totalSeats = rows * numberOfSeats;
         int incomeFrontHalf;
+        int frontHalfSeats;
         int incomeBackHalf;
         int income;
         final int frontPrice = 10;
@@ -45,19 +56,63 @@ public class Main {
             income = totalSeats * 10;
         } else {
 
+            // Runs if total number of rows are even
             if (rows % 2 == 0) {
                 incomeFrontHalf = ((rows / 2) * numberOfSeats) * frontPrice;
+                frontHalfSeats = rows / 2;
                 incomeBackHalf = ((rows / 2) * numberOfSeats) * backPrice;
                 income = incomeFrontHalf + incomeBackHalf;
+
+                if (chosenRow <= frontHalfSeats) {
+                    System.out.println("Ticket price: $" + frontPrice);
+                } else {
+                    System.out.println("Ticket price: $" + backPrice);
+                }
             } else {
                 incomeFrontHalf = ((rows / 2) * numberOfSeats) * frontPrice;
+                frontHalfSeats = rows / 2;
                 incomeBackHalf = ((rows / 2 + 1) * numberOfSeats) * backPrice;
                 income = incomeFrontHalf + incomeBackHalf;
+
+                if (chosenRow <= frontHalfSeats) {
+                    System.out.println("Ticket price: $" + frontPrice);
+                } else {
+                    System.out.println("Ticket price: $" + backPrice);
+                }
             }
         }
 
         System.out.print("Total income: ");
         System.out.println("$" + income);
+        System.out.println();
+
+
+        // Part Three: Get Ticket Price and Shows the Picked Seat
+
+        String[][] seatingArray = new String[rows][numberOfSeats];
+
+        System.out.println("Cinema:");
+        // Redraws the seating map
+        System.out.print("  ");
+        for (int col = 1; col <= numberOfSeats; col++) {
+            System.out.print(col + " ");
+        }
+        System.out.println();
+        rowCounter = 1;
+        for (int newRow = 0; newRow < seatingArray.length; newRow++) {
+            System.out.print(rowCounter + " ");
+            for (int singleSeat = 0; singleSeat < seatingArray[newRow].length; singleSeat++) {
+                if (newRow + 1 == chosenRow && singleSeat + 1 == chosenSeat) {
+                    // This targets the seat coordinates the user chose to sit in
+                    seatingArray[newRow][singleSeat] = "B";
+                } else {
+                    seatingArray[newRow][singleSeat] = "S";
+                }
+                System.out.print(seatingArray[newRow][singleSeat] + " ");
+            }
+            System.out.println();
+            rowCounter++;
+        }
 
 
     }
