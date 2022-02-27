b5bcfca56f2706761b53dbeee2b002ba5e3e6f92
joacotol <joacochrist@gmail.com>
2022-02-27 16:35:19 -0500

    Finished the final stage of the project. Made the program more resistant to errors, created new methods for readability.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,33 +3,37 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static int totalIncome;
+    private static int currentIncome;
+
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
-        // Part One: Cinema Visualization
-        System.out.print("Enter number of rows: ");
+        System.out.println("Enter the number of rows: ");
         int rows = userInput.nextInt();
-        System.out.print("Enter the number of seats per row: ");
+        System.out.println("Enter the number of seats in each row: ");
         int numberOfSeats = userInput.nextInt();
 
         String[][] seatingArray = new String[rows][numberOfSeats];
+        // Draws the cinema so the array is not empty
+        createCinema(seatingArray);
+        // Calculates the total income if all seats are sold
+        setTotalIncome(rows, numberOfSeats);
+        // Shows the main menu
         showMenu(seatingArray, rows, numberOfSeats);
 
     }
 
     public static void showMenu(String[][] seatingArray, int rows, int numberOfSeats) {
-        Scanner scanner = new Scanner(System.in);
-        // Main menu of the program
+        Scanner userInput = new Scanner(System.in);
+        // This loop will re-iterate until the user exits the program
         while (true) {
             System.out.println("1. Show the seats");
             System.out.println("2. Buy a ticket");
+            System.out.println("3. Statistics");
             System.out.println("0. Exit");
 
-            int input = scanner.nextInt();
-            // Exits the program
-            if (input == 0) {
-                break;
-            }
+            int input = userInput.nextInt();
 
             switch (input) {
                 case 1:
@@ -39,60 +43,169 @@ public class Main {
                     getTicketPrice(seatingArray, rows, numberOfSeats);
                     System.out.println();
                     break;
+                case 3:
+                    statistics(seatingArray);
+                    break;
+                // Exits the program
                 case 0:
                     return;
             }
         }
+
     }
 
+    public static void statistics(String[][] seatingArray) {
+        System.out.printf("\nNumber of purchased tickets: %d\n", getTakenSeats(seatingArray));
+        System.out.printf("Percentage: %.2f%%\n", getPercentageOfSeats(seatingArray));
+        System.out.printf("Current income: $%d\n", getIncome());
+        System.out.printf("Total income: $%d\n\n", getTotalIncome());
+    }
+
+    // Returns the number of seats sold
+    public static int getTakenSeats(String[][] seatingArray) {
+        int takenSeats = 0;
+        for (String[] strings : seatingArray) {
+            for (String string : strings) {
+                if (string.equals("B")) {
+                    takenSeats++;
+                }
+            }
+        }
+        return takenSeats;
+    }
+
+    // Returns the amount of seats compared to the total seats by percentage
+    public static double getPercentageOfSeats(String[][] seatingArray) {
+        int totalSeats = 0;
+        for (String[] row : seatingArray) {
+            for (String seat : row) {
+                totalSeats++;
+            }
+        }
+        int totalTakenSeats = getTakenSeats(seatingArray);
+
+        return (1.0 * totalTakenSeats / totalSeats) * 100 ;
+    }
+
+    public static void createCinema(String[][] seatingArray) {
+        for (int i = 0; i < seatingArray.length; i++) {
+            for (int j = 0; j < seatingArray[i].length; j++) {
+                seatingArray[i][j] = "S";
+            }
+        }
+    }
+
+    // Outputs the full cinema that shows the column and row numbers
     public static void showCinema(String[][] seatingArray, int numberOfSeats) {
         int rowCounter = 1;
 
         System.out.println("Cinema:");
-        // Draws the header column of the seating map
+        // Draws Columns
         System.out.print("  ");
-        for (int column = 1; column <= numberOfSeats; column++) {
-            System.out.print(column + " ");
+        for (int i = 1; i <= numberOfSeats; i++) {
+            System.out.print(i + " ");
         }
         System.out.println();
 
+        // Draws the "S" as long as it is null or doesn't have "B"
         // Draws the rest of the seating map
-        // It will print out "S" for available seats and "B" for taken seats
-        for (int row = 0; row < seatingArray.length; row++) {
+        for (int j = 0; j < seatingArray.length; j++) {
             System.out.print(rowCounter + " ");
-            for (int singleSeat = 0; singleSeat < seatingArray[row].length; singleSeat++) {
-                if (seatingArray[row][singleSeat] == null || !(seatingArray[row][singleSeat].equals("B"))) {
+            for (int singleSeat = 0; singleSeat < seatingArray[j].length; singleSeat++) {
+                if (seatingArray[j][singleSeat] == null || !(seatingArray[j][singleSeat].equals("B"))) {
                     // This targets the seat coordinates the user chose to sit in
-                    seatingArray[row][singleSeat] = "S";
+                    seatingArray[j][singleSeat] = "S";
                 }
-                System.out.print(seatingArray[row][singleSeat] + " ");
+                System.out.print(seatingArray[j][singleSeat] + " ");
             }
             System.out.println();
             rowCounter++;
         }
+
     }
 
     public static void getTicketPrice(String[][] seatingArray, int rows, int numberOfSeats) {
-        Scanner scanner = new Scanner(System.in);
-        // Asks the user their chosen seat
-        System.out.println("Enter a row number:");
-        int chosenRow = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int chosenSeat = scanner.nextInt();
+        Scanner userInput = new Scanner(System.in);
+        // Will keep re-iterating until the user enters valid seat numbers
+        while (true) {
+            // Asks the user their chosen seat
+            System.out.println("\nEnter a row number:");
+            int chosenRow = userInput.nextInt();
+            System.out.println("Enter a seat number in that row:");
+            int chosenSeat = userInput.nextInt();
 
-        int totalSeats = rows * numberOfSeats;
-        // This outputs the price of an individual ticket based on its position
-        // If the total seats in the cinema is less than 60 then the price for all tickets is 10$
-        // Else if the chosen seat is located in front half of the cinema the ticket price is 10$
-        // Else the price of a ticket is 8$ if the chosen seat is in the back of the cinema
-        System.out.println("Ticket price: $" + (totalSeats <= 60 ? 10 : chosenRow <= rows / 2 ? 10 : 8));
+            // If user enters out of bounds values
+            if (chosenRow > rows || chosenSeat > numberOfSeats) {
+                System.out.println("\nWrong input!");
+                continue;
+            }
 
-        for (int i = 0; i < seatingArray.length; i++) {
-            for (int j = 0; j < seatingArray[i].length; j++) {
-                if (i == chosenRow - 1 && j == chosenSeat -1) {
-                    seatingArray[i][j] = "B";
+            int totalSeats = rows * numberOfSeats;
+
+            // This outputs the price of a ticket based on its position
+            // If the total seats in the cinema is less than 60 then the price for all tickets is 10$
+            // Else if the chosen seat is located at the front half of the cinema, ticket price is 10$
+            // Else the price of a ticket is 8$ if the chosen seat is at the back of the cinema
+            int priceOfTicket = totalSeats <= 60 ? 10 : chosenRow <= rows / 2 ? 10 : 8;
+            System.out.println("\nTicket price: $" + priceOfTicket);
+            boolean alreadyTaken = false;
+            // Will change the chosen seat to taken as long as it's not already taken
+            for (int i = 0; i < seatingArray.length && !alreadyTaken; i++) {
+                for (int j = 0; j < seatingArray[i].length; j++) {
+                    if (i == chosenRow - 1 && j == chosenSeat - 1) {
+                        if (seatingArray[i][j].equals("B")) {
+                            System.out.println("\nThat ticket has already been purchased!");
+                            alreadyTaken = true;
+                            break;
+                        } else {
+                            seatingArray[i][j] = "B";
+                            // Will add the price of the ticket bought to the income made
+                            calculateIncome(priceOfTicket);
+                        }
+                    }
                 }
             }
+            // Will break the loop if the seat chosen is not taken
+            if (!alreadyTaken) {
+                break;
+            }
         }
+
+    }
+
+    public static void calculateIncome(int priceOfTicket) {
+        currentIncome += priceOfTicket;
+    }
+
+    public static void setTotalIncome(int rows, int numberOfSeats) {
+        final int totalSeats = rows * numberOfSeats;
+        int incomeFrontHalf;
+        int incomeBackHalf;
+        int income;
+        final int frontPrice = 10;
+        final int backPrice = 8;
+
+        if (totalSeats < 60) {
+            income = totalSeats * 10;
+        } else {
+            if (rows % 2 == 0) {
+                incomeFrontHalf = ((rows / 2) * numberOfSeats) * frontPrice;
+                incomeBackHalf = ((rows / 2) * numberOfSeats) * backPrice;
+                income = incomeFrontHalf + incomeBackHalf;
+            } else {
+                incomeFrontHalf = ((rows / 2) * numberOfSeats) * frontPrice;
+                incomeBackHalf = ((rows / 2 + 1) * numberOfSeats) * backPrice;
+                income = incomeFrontHalf + incomeBackHalf;
+            }
+        }
+        totalIncome = income;
+    }
+
+    public static int getTotalIncome() {
+        return totalIncome;
+    }
+
+    public static int getIncome() {
+        return currentIncome;
     }
 }
